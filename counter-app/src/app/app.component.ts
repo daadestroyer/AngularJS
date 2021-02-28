873ebdddf761f6182a5b2125b5502d83323cc511
daadestroyer <nigamshubham2000@gmail.com>
2021-02-28 18:34:29 +0530

    1st app counter complted...

--- counter-app/src/app/app.component.ts
+++ counter-app/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title:string = "Counter App";
+  footer:string = "Design and developed by Shubham Nigam aka"
+  footername:string = "daadestroyer"
+
+  count:number = 0
+ 
+  // To increase count value
+  increaseHandler = ()=>{
+    if(this.count == 10){
+      this.count+=15;
+    }
+    this.count += 1;
+  }
+
+  // To decrease count value
+  decreaseHandler = ()=>{
+    if(this.count<=0){
+      alert('cant be less than 0')
+      return
+    }
+    else{
+      this.count -= 1;
+    }
+    
+  }
+
+  // To rest count value
+  resetHandler = ()=>{
+    this.count = 0;
+  }
+
 }
